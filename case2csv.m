06d95b19cf71c7c359eca604b7af6f7996194812
Richard Lincoln <r.w.lincoln@gmail.com>
2023-08-07 11:46:13 +0200

    Adding final objective function value field.

--- case2csv.m
+++ case2csv.m
@@ -22,23 +22,33 @@ else
   baseMVA = 1.0;
 endif
 
+if isfield(mpc, 'f')
+  f = mpc.f;
+endif
+
 if (ischar (file))
   [fid, msg] = fopen (file, "wt");
 elseif (is_valid_file_id (file))
   [fid, msg] = deal (file, "invalid file number");
 else
-  error ("case2csv: FILE must be a filename string or numeric FID");
+  error("case2csv: FILE must be a filename string or numeric FID");
 endif
 
 if (fid < 0)
-  error (["case2csv: " msg]);
+  error(["case2csv: " msg]);
 endif
 
 case_header = strjoin({"CASENAME", "VERSION", "BASE_MVA"}, delim);
+case_header_opf = strjoin({"CASENAME", "VERSION", "BASE_MVA", "F"}, delim);
 
-fprintf(fid, "%s\n", case_header);
-fprintf(fid, "%s%s%s%s%g\n", casename, sprintf(delim), version, sprintf(delim), baseMVA);
+if isfield(mpc, 'f')
+  fprintf(fid, "%s\n", case_header_opf);
+  fprintf(fid, "%s%s%s%s%g%s%g\n", casename, sprintf(delim), version, sprintf(delim), baseMVA, sprintf(delim), f);
+elseif
+  fprintf(fid, "%s\n", case_header);
+  fprintf(fid, "%s%s%s%s%g\n", casename, sprintf(delim), version, sprintf(delim), baseMVA);
+end
 
-if (ischar (file))
+if (ischar(file))
   fclose (fid);
 endif
